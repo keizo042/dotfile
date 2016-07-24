20344bbdb790c7787f97f555d1f960a75f1e7791
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-07-24 15:31:04 +0900

    UPDATE

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@
 ln -s $HOME/.conf/dotfile/bash $HOME/.bashrc
 ln -s $HOME/.conf/dotfile/vim/templates $HOME/.vim/templates
 ln -s $HOME/.conf/dotfile/vimrc $HOME/.vimrc
+touch $HOME/.bashrc.local
 # ln -s $HOME/.conf/dotfile/xmonad.hs $HOME/.monad/xmonad.hs
