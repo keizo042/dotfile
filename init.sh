a5fe57dbb931eecfc85f22e43ff3019861ff203a
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-07-25 09:18:38 +0900

    UPDATE

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-ln -s $HOME/.conf/dotfile/bash $HOME/.bashrc
-ln -s $HOME/.conf/dotfile/vim/templates $HOME/.vim/templates
-ln -s $HOME/.conf/dotfile/vimrc $HOME/.vimrc
-# ln -s $HOME/.conf/dotfile/xmonad.hs $HOME/.monad/xmonad.hs
+ln -s $HOME/.config/dotfile/bash $HOME/.bashrc
+mkdir -p $HOME/.vim/templates
+ln -s $HOME/.config/dotfile/vim/templates $HOME/.vim/templates
+ln -s $HOME/.config/dotfile/vimrc $HOME/.vimrc
+# ln -s $HOME/.config/dotfile/xmonad.hs $HOME/.monad/xmonad.hs
