3cb6a5d2f4456723204e8f8139dd86d3f103c63b
Koichi Nakanishi <keizo042dev@gmail.com>
2018-08-11 01:09:56 +0900

    UPDATE

--- bash/alias.bash
+++ bash/alias.bash
@@ -12,21 +12,28 @@ alias c='git checkout `git branch | peco | sed -e "s/\* //g" | awk "{print \$1}"
 alias vhasktag="hasktags --ignore-close-implementation --ctags"
 alias phptag='ctags -R --regex-php="/^[ \t]*const[ \t]+([a-z0-9_]+)/\1/d/i"'
 
-if [[ "$(uname)" == "Linux" ]]; then
 
+function aliasLinuxDefinie () {
 alias l='ls -F --color=auto'
 alias ls='ls -F --color=auto'
 alias sl='ls -F --color=auto'
 alias la='ls -aF --color=auto'
 alias ll='ls -lF --color=auto'
 alias xbset='xbacklight -set'
+}
 
-else
+function aliasDarwinDefine () {
 
 alias l='ls -F'
 alias ls='ls -F'
 alias sl='ls -F'
 alias la='ls -aF'
 alias ll='ls -lF'
+}
 
+if [[ "$(uname)" == "Darwin" ]]; then
+  aliasDarwinDefine
+
+else
+  aliasLinuxDefinie
 fi
